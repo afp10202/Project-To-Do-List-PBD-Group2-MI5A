139b760025394df37b55f5de711c05adc4dc6c92
garniswaningsiun <garnisbaturaja123@gmail.com>
2023-10-05 19:12:00 +0700

    Update lagi terbaru TodolistRepository

--- Repository/TodolistRepository.php
+++ Repository/TodolistRepository.php
@@ -15,21 +15,22 @@ namespace Repository {
 
     }
 
-    class TodolistRepositoryImpl implements TodolistRepository {
+    class TodolistRepositoryImpl implements TodolistRepository
+    {
 
         public array $todolist = array();
 
         private \PDO $connection;
 
-        public function __construct(\PDO $connection){
-
+        public function __construct(\PDO $connection)
+        {
             $this->connection = $connection;
         }
 
         function save(Todolist $todolist): void
         {
-            //$number = sizeof($this->todolist) + 1;
-            //$this->todolist[$number] = $todolist;
+            // $number = sizeof($this->todolist) + 1;
+            // $this->todolist[$number] = $todolist;
 
             $sql = "INSERT INTO todolist(todo) VALUES (?)";
             $statement = $this->connection->prepare($sql);
@@ -38,22 +39,52 @@ namespace Repository {
 
         function remove(int $number): bool
         {
-            if ($number > sizeof($this->todolist)) {
+//            if ($number > sizeof($this->todolist)) {
+//                return false;
+//            }
+//
+//            for ($i = $number; $i < sizeof($this->todolist); $i++) {
+//                $this->todolist[$i] = $this->todolist[$i + 1];
+//            }
+//
+//            unset($this->todolist[sizeof($this->todolist)]);
+//
+//            return true;
+
+            $sql = "SELECT id FROM todolist WHERE id = ?";
+            $statement = $this->connection->prepare($sql);
+            $statement->execute([$number]);
+
+            if ($statement->fetch()) {
+                // todolist ada
+                $sql = "DELETE FROM todolist WHERE id = ?";
+                $statement = $this->connection->prepare($sql);
+                $statement->execute([$number]);
+                return true;
+            } else {
+                // todolist tidak ada
                 return false;
             }
-
-            for ($i = $number; $i < sizeof($this->todolist); $i++) {
-                $this->todolist[$i] = $this->todolist[$i + 1];
-            }
-
-            unset($this->todolist[sizeof($this->todolist)]);
-
-            return true;
         }
 
         function findAll(): array
         {
-            return $this->todolist;
+            // return $this->todolist;
+            $sql = "SELECT id, todo FROM todolist";
+            $statement = $this->connection->prepare($sql);
+            $statement->execute();
+
+            $result = [];
+
+            foreach ($statement as $row) {
+                $todolist = new Todolist();
+                $todolist->setId($row['id']);
+                $todolist->setTodo($row['todo']);
+
+                $result[] = $todolist;
+            }
+
+            return $result;
         }
     }
 

5a83868469c4706c84513ce1e44e32146757a750
AndriFP <113114423+andrifp@users.noreply.github.com>
2023-10-06 13:56:13 +0700

    fix bug 2

--- App.php
+++ App.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once _DIR_ . '/Entity/Todolist.php';
-require_once _DIR_ . '/Helper/InputHelper.php';
-require_once _DIR_ . '/Repository/TodolistRepository.php';
-require_once _DIR_ . '/Service/TodolistService.php';
-require_once _DIR_ . '/View/TodolistView.php';
-require_once _DIR_ . '/Config/Database.php';
-
+require_once __DIR__ . '/Entity/Todolist.php';
+require_once __DIR__ . '/Helper/InputHelper.php';
+require_once __DIR__ . '/Repository/TodolistRepository.php';
+require_once __DIR__ . '/Service/TodolistService.php';
+require_once __DIR__ . '/View/TodolistView.php';
+require_once __DIR__ . '/Config/Database.php';
+ 
 use Repository\TodolistRepositoryImpl;
 use Service\TodolistServiceImpl;
 use View\TodolistView;
@@ -19,3 +19,4 @@ $todolistService = new TodolistServiceImpl($todolistRepository);
 $todolistView = new TodolistView($todolistService);
 
 $todolistView->showTodolist();
+
